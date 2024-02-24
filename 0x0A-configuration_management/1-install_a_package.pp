5d2b54c1be941cddadeb23fde71c106701b60561
balmed <iwymed@gamil.com>
2024-02-24 13:41:11 -0800

    modify t 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,10 @@
 # Install package flask
-package { 'Flask':
-  ensure   => 'version',
-  provider => pip3,
+package { 'flask':
+  ensure    =>  '2.1.0',
+  provider  =>  'pip3', 
+}
+
+package { 'Werkzeug':
+  ensure    =>  '2.1.1',
+  provider  =>  'pip3', 
 }

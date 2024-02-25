c6c0973aa513a1bb12986709f970cd9634dce100
balmed <iwymed@gamil.com>
2024-02-25 06:47:25 -0800

    task adv

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+# using puppet to make changes to the default ssh config file
+# so that one can connect to a server without typing a password.
+
+file_line { 'etc/ssh/ssh-config':
+  ensure	=>present,
+  content	=>"
+		#SSH Client configuration
+		host*
+		IdentityFile ~/.ssh/school
+		PasswordAuthentication no
+		",
+}

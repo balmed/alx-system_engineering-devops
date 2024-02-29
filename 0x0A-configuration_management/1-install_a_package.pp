4af35b843b581ecaf6edee15659b6d33f744c85b
balmed <iwymed@gamil.com>
2024-02-28 16:13:23 -0800

    task 1m

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,17 +2,11 @@
 # Install package flask
 
 package { 'flask':
-  ensure    =>  '2.1.0',
-  provider  =>  'pip3', 
-}
-notify { 'Flask installed':
-  message => 'Flask package installation completed.',
+  ensure   => '2.1.0',
+  provider => 'pip3', 
 }
 
 package { 'Werkzeug':
-  ensure    =>  '2.1.1',
-  provider  =>  'pip3', 
-}
-notify { 'Werkzeug installed':
-  message => 'Werkzeug package installation completed.',
+  ensure    => '2.1.1',
+  provider  => 'pip3', 
 }

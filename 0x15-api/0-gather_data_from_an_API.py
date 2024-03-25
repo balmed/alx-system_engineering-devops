d5bbd6944cc1cb9e70d6c45ecc626f43c575dbf6
balmed <iwymed@gamil.com>
2024-03-25 16:17:03 -0700

    task adm

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,24 +7,24 @@ import re
 import requests
 import sys
 
-REST_API = "https://jsonplaceholder.typicode.com/todos/1"
+REST_API = "https://jsonplaceholder.typicode.com"
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         if re.fullmatch(r'\d+', sys.argv[1]):
             id = int(sys.argv[1])
-            req = requests.get('{}/users/{}'.format(REST_API, id)).json()
-            task_req = requests.get('{}/todos'.format(REST_API)).json()
-            emp_name = req.get('name')
-            tasks = list(filter(lambda x: x.get('userId') == id, task_req))
-            completed_tasks = list(filter(lambda x: x.get('completed'), tasks))
+            rq_u = requests.get('{}/users/{}'.format(REST_API, id)).json()
+            tk_req = requests.get('{}/todos'.format(REST_API)).json()
+            emp_name = rq_u.get('name')
+            tks = list(filter(lambda x: x.get('userId') == id, tk_req))
+            completed_tasks = list(filter(lambda x: x.get('completed'), tks))
             print(
                 'Employee {} is done with tasks({}/{}):'.format(
                     emp_name,
                     len(completed_tasks),
-                    len(tasks)
+                    len(tks)
                 )
             )
             if len(completed_tasks) > 0:
-                for task in completed_tasks:
-                    print('\t {}'.format(task.get('title'))
+                for tk in completed_tasks:
+                    print('\t {}'.format(tk.get('title')))
---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Export api to csv"""
+import csv
+import requests
+import sys
+
+if __name__ == '__main__':
+    user = sys.argv[1]
+    url_user = 'https://jsonplaceholder.typicode.com/users/' + user
+    res = requests.get(url_user)
+    """ANYTHING"""
+    user_name = res.json().get('username')
+    tk = url_user + '/todos'
+    res = requests.get(task)
+    tks = res.json()
+
+    with open('{}.csv'.format(user), 'w') as csvfile:
+        for tk in tks:
+            completed = tk.get('completed')
+            """Complete"""
+            title_tk = tk.get('title')
+            """Done"""
+            csvfile.write('"{}","{}","{}","{}"\n'.format(
+                user, user_name, completed, title_tk))

3a4964931b4e1aa81638f713544ee044945d3103
balmed <iwymed@gamil.com>
2024-03-25 18:39:02 -0700

    task 0 md

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -14,17 +14,17 @@ if __name__ == '__main__':
         if re.fullmatch(r'\d+', sys.argv[1]):
             id = int(sys.argv[1])
             req = requests.get('{}/users/{}'.format(REST_API, id)).json()
-            task_req = requests.get('{}/todos'.format(REST_API)).json()
+            tk_req = requests.get('{}/todos'.format(REST_API)).json()
             emp_name = req.get('name')
-            tasks = list(filter(lambda x: x.get('userId') == id, task_req))
-            completed_tasks = list(filter(lambda x: x.get('completed'), tasks))
+            tks = list(filter(lambda x: x.get('userId') == id, tk_req))
+            completed_tasks = list(filter(lambda x: x.get('completed'), tks))
             print(
                 'Employee {} is done with tasks({}/{}):'.format(
                     emp_name,
                     len(completed_tasks),
-                    len(tasks)
+                    len(tks)
                 )
             )
             if len(completed_tasks) > 0:
-                for task in completed_tasks:
-                    print('\t {}'.format(task.get('title')))
+                for tk in completed_tasks:
+                    print('\t {}'.format(tk.get('title')))

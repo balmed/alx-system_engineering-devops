8a75c02df3dbeb6a21bf6608deeea2a70c6aa6a6
balmed <iwymed@gamil.com>
2024-04-09 19:28:04 -0700

    task0 mod

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,26 @@
-"""
-Script that queries subscribers on a given Reddit subreddit.
-"""
+#!/usr/bin/python3
 
+"""[task 0, get rest api]
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
+    """returns the number of subscribers
+
+    Args:
+        subreddit ([string]): [type of subreddit to consult]
+
+    Returns:
+        [int]: [# of subscribers]
+    """
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    about_info = requests.get(url, headers={"User-Agent": "dairof7"},
+                              allow_redirects=False)
+    if about_info.status_code == 200:
+        return about_info.json()['data']['subscribers']
     else:
         return 0
+
+if __name__ == "__main__":
+    pass
